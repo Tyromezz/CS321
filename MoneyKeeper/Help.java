3a0001137f482a8ae3cea4d196d3df8ce7ca8ddf
Pooja Sharma <poojasharma@Poojas-Air.byod.gmu.edu>
2019-04-17 14:44:03 -0400

    Updated MoneyKeeper with Help button, formatted Expenses/Incomes Button

--- MoneyKeeper/Help.java
+++ MoneyKeeper/Help.java
@@ -9,38 +9,35 @@ public class Help {
  private Object requestedClass;
  
  // Help Constructor.
- public Help(Object c) {
-   this.requestedClass = c;
+ public Help() {
+   //this.requestedClass = c;
  }
  
  
- public String getHelpMessage() {
+ public String getHelpMessage(Object requestedClass) {
    String message = "";
-   if (this.requestedClass instanceof Expenses) {
+   if (requestedClass instanceof Expenses) {
      //Display Instructions for Expenses Class. 
      message = "The Expenses feature allows you to add planned and unplanned expenses to your Financial Planner.\n To add a new expense, please enter the cost of the expense.\nThen select the type of expense by choosing from the expenses drop-down menu.\nFinally, click 'Add Expense' to add the new expense.";
    }
    
-   else if (this.requestedClass instanceof Incomes){
+   else if (requestedClass instanceof Incomes){
      //Display Instructions for Incomes Class. 
      message = "The Incomes feature allows you to add multiple streams of incomes (Primary, Secondary, Side Job, Hobby, Other) to your Financial Planner.\n To add a new income, please enter the amount of income you make.\nThen select the type of income by choosing from the incomes drop-down menu.\nFinally, click 'Add Income' to add the new income..";
-
    }
-   
-   else if (this.requestedClass instanceof StudentDebt){
+   /*
+   else if (requestedClass instanceof StudentDebt){
      //Display Instructions for Student Debt Class. 
-     message = "The Student Debt feature allows you to add multiple student loans to your Financial Planner.\n To add a new loan, please enter the amount of load you took out.\nThen select the type of loan by choosing from the loans drop-down menu.\nFinally, click 'Add Loan' to add the new loan.";
-     
+     message = "The Student Debt feature allows you to enter multiple student loans to your Financial Planner.\n To add a new loan, please enter the amount of load you took out.\nThen select the type of loan by choosing from the loans drop-down menu.\nFinally, click 'Add Loan' to add the new loan."; 
    }
-   
+   */
    else {
      // Display Instructions for Investments Class. We might not need this as Investments is something the 
      // program decides based on how much money you have available to spend, your student debt amount etc. 
      message = "The Investments feature suggests multiple investments options based on your current income, total expenses and student loan amount."; 
    }
-   
+   System.out.println(message);
    return message;
-   
  }
  
 }
--- MoneyKeeper/MoneyKeeper.java
+++ MoneyKeeper/MoneyKeeper.java
@@ -4,56 +4,102 @@ import java.awt.Component;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.awt.event.ActionEvent;
+import java.awt.FlowLayout;
 
 public class MoneyKeeper {
-	
-	public ArrayList<JPanel> panelList = new ArrayList<>();
-	
-	public static void main(String[] args) {
-		new MoneyKeeper();
-	}
+ 
+ public ArrayList<JPanel> panelList = new ArrayList<>();
+  public Incomes inc;
+ public Expenses exp;
+ public Help help;
+ 
+ public static void main(String[] args) {
+  new MoneyKeeper();
+ }
 
-	public MoneyKeeper() {
-		JFrame guiFrame = new JFrame();
-		guiFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		guiFrame.setTitle("MoneyKeeper");
-		guiFrame.setSize(850, 800);
-		//This will center the JFrame in the middle of the screen
-		guiFrame.setLocationRelativeTo(null);
-	
-		//Expense Options
-		String[] expenses = { "Rent", "Gas", "Insurance", "Food", "Utilities", "Entertainment"};
-		
-		//The first JPanel instantiates an Incomes Class
-		JPanel incomePanel = new JPanel();
-		Incomes inc = new Incomes(incomePanel);
-		this.panelList.add(incomePanel);
-		
-		
-		//The second JPanel instantiates an Expenses Class
-		//make suse the JPanel is not visible at start.
-		JPanel expensePanel = new JPanel();
-		Expenses exp = new Expenses(expensePanel);
-		this.panelList.add(expensePanel);
-		expensePanel.setVisible(false);
-		
-		
-		
-		JButton changeScreen = new JButton("Incomes or Expenses");
-		changeScreen.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent event) {
-				expensePanel.setVisible(!expensePanel.isVisible());
-				incomePanel.setVisible(!incomePanel.isVisible());
-			}
-		});
-		
-		//The JFrame uses the BorderLayout layout manager.
-		//Put the two JPanels and JButton in different areas.
-		guiFrame.add(incomePanel, BorderLayout.NORTH);
-		guiFrame.add(expensePanel, BorderLayout.CENTER);
-		guiFrame.add(changeScreen, BorderLayout.SOUTH);
-		//make sure the JFrame is visible
-		guiFrame.setVisible(true);
-	}
+ public MoneyKeeper() {
+  JFrame guiFrame = new JFrame();
+  guiFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  guiFrame.setTitle("MoneyKeeper");
+  guiFrame.setSize(850, 800);
+  //This will center the JFrame in the middle of the screen
+  guiFrame.setLocationRelativeTo(null);
+ 
+  //Expense Options
+  String[] expenses = { "Rent", "Gas", "Insurance", "Food", "Utilities", "Entertainment"};
+  
+  //The first JPanel instantiates an Incomes Class
+  JPanel incomePanel = new JPanel();
+  this.inc = new Incomes(incomePanel);
+  this.panelList.add(incomePanel);
+  this.help = new Help();
+  
+  
+  
+  //The second JPanel instantiates an Expenses Class
+  //make suse the JPanel is not visible at start.
+  JPanel expensePanel = new JPanel();
+  this.exp = new Expenses(expensePanel);
+  this.panelList.add(expensePanel);
+  expensePanel.setVisible(false);
+  
+  
+  
+  JButton expenseScreen = new JButton("Expenses");
+  expenseScreen.addActionListener(new ActionListener() {
+   @Override
+   public void actionPerformed(ActionEvent event) {
+    expensePanel.setVisible(true);
+    incomePanel.setVisible(false);
+   }
+  });
+  
+  JButton incomeScreen = new JButton("Incomes");
+  incomeScreen.addActionListener(new ActionListener() {
+   @Override
+   public void actionPerformed(ActionEvent event) {
+    expensePanel.setVisible(false);
+    incomePanel.setVisible(true);
+   }
+  });
+  
+  JButton helpButton = new JButton("Help");
+  incomeScreen.addActionListener(new ActionListener() {
+   @Override
+   public void actionPerformed(ActionEvent event) {
+     if (incomePanel.isVisible()){
+       help.getHelpMessage(Incomes.class);
+     }}
+  });
+  
+  
+ 
+  
+  JPanel buttonPanel = new JPanel();
+  buttonPanel.add(incomeScreen);
+  buttonPanel.add(expenseScreen);
+  buttonPanel.setLayout(new FlowLayout());
+  buttonPanel.add(helpButton);
+  guiFrame.add(buttonPanel, BorderLayout.SOUTH);
+  
+  
+
+  
+  //The JFrame uses the BorderLayout layout manager.
+  //Put the two JPanels and JButton in different areas.
+  guiFrame.setLayout(new FlowLayout());
+  guiFrame.add(incomePanel, BorderLayout.CENTER);
+  guiFrame.add(expensePanel, BorderLayout.CENTER);
+  //guiFrame.add(expenseScreen, BorderLayout.SOUTH);
+  //guiFrame.add(incomeScreen, BorderLayout.SOUTH);
+  //make sure the JFrame is visible
+  guiFrame.setVisible(true);
+ }
+    // Calculate the difference between income and expenses. Used in multiple
+ // functions throughout the application. Allows program to plan for various extra investments,
+ // suggest student debt pay off plan etc. 
+ public int calculateDelta() {
+   int delta = this.inc.sumIncomes() - this.exp.sumExpenses();
+   return delta;
+ }
 }
