a7c739fb014ee8237e7dea7efa19c2e24c86776b
Pooja Sharma <poojasharma@Poojas-MacBook-Air.local>
2019-04-27 15:14:06 -0400

    Added Testing Suites for MoneyKeeper. Will need a seperate test file for each class.

---
+++ MoneyKeeper/InvestmentsTests.java
@@ -0,0 +1,18 @@
+import org.junit.*;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import java.util.*;
+
+public class InvestmentsTests {
+  public static void main(String args[]){
+    org.junit.runner.JUnitCore.main("InvestmentsTests");
+  }
+  
+  static MoneyKeeper mk_object = new MoneyKeeper(); 
+
+  
+  
+  // Investments Tests
+  @Test (timeout=2000) public void test_Investments_1(){ assertEquals("You have a total of $%d free to use in your account.\nCurrently, we do not suggest you invest your money in anything.", mk_object.invest.investmentSuggestion()); }
+
+}
---
+++ MoneyKeeper/MoneyKeeperTestSuites.java
@@ -0,0 +1,16 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.util.*;
+import java.util.Arrays.*;
+
+public class MoneyKeeperTestSuites {
+  public static void main(String args[]){
+    org.junit.runner.JUnitCore.main("MKTests",  // Testing for MoneyKeeper class. various methods etc.
+                                    "IncomesTests",
+                                    "ExpensesTests",
+                                    "StudentDebtTests",
+                                    "InvestmentsTests",
+                                    "HelpTests"
+                                   );
+  }
+}
