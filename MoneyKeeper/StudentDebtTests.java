a94032a2d5db25738fbd81f8d319421b84c775a4
Pooja Sharma <poojasharma@Poojas-MacBook-Air.local>
2019-04-28 17:26:53 -0400

    Karl updated his StudentDebtTests.java to full implementation

--- MoneyKeeper/StudentDebtTests.java
+++ MoneyKeeper/StudentDebtTests.java
@@ -4,15 +4,28 @@ import static org.junit.Assert.*;
 import java.util.*;
 
 public class StudentDebtTests {
-  public static void main(String args[]){
-    org.junit.runner.JUnitCore.main("StudentDebtTests");
-  }
-  
-  static MoneyKeeper mk_object = new MoneyKeeper(); 
+    public static void main(String args[]){
+        org.junit.runner.JUnitCore.main("StudentDebtTests");
+    }
 
-  
-  
-  // Student Debt  Tests
-  @Test (timeout=2000) public void test_StudentDebt_1(){ }
+    static MoneyKeeper mk_object = new MoneyKeeper();
+
+    // Student Debt addDebt Test
+    @Test (timeout=2000) public void test_StudentDebt_1() {
+        MoneyKeeper mk_object2 = new MoneyKeeper();
+        assertEquals(0, mk_object2.debt.addDebt(0));
+        assertEquals(100, mk_object2.debt.addDebt(100));
+    }
+
+    // Student Debt sumDebt Test
+    @Test (timeout=2000) public void test_StudentDebt_2(){
+        assertEquals(0, mk_object.debt.sumStudentDebt());
+        mk_object.debt.addDebt(50);
+        assertEquals(50, mk_object.debt.sumStudentDebt());
+        mk_object.debt.addDebt(112);
+        assertEquals(162, mk_object.debt.sumStudentDebt());
+        mk_object.debt.addDebt(28974);
+        assertEquals(29136, mk_object.debt.sumStudentDebt());
+    }
 
 }
